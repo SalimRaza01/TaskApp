494e8e919bcc1bb20421db81a0023cea94262afe
Salim Raza <108859692+SalimRaza01@users.noreply.github.com>
2023-11-21 17:21:54 +0530

    Update TaskSchema.js

--- server/TaskSchema.js
+++ server/TaskSchema.js
@@ -5,7 +5,7 @@ const TaskSchema = new mongoose.Schema({
   description: String,
   status: String,
   deadline: Date,
-  createdAt: Date,
+  createdAt: {type: Date, default: Date.now()},
   comments: [
     {
       commenterEmail: String,
